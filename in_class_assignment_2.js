9db0fd9c0997048054cdb9adfa6f7b82323eaf9f
cjwattttts <cameron.jwatts@outlook.com>
2025-02-11 16:05:35 -0500

    TAKS 1

---
+++ in_class_assignment_2.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+// TASK 1
+
+let products = [
+    { name: "Laptop", price: 1200, category: "Electronics" }, // T
+    { name: "Phone", price: 800, category: "Electronics" }, // T
+    { name: "Desk Chair", price: 150, category: "Furniture" } // F
+];
+
+function getProductsByCategory(products, category) {
+    return product_array.filter(product => product.category === category_name);
+}
+
+console.log(getProductsByCategory(products, "Electronics"));
+
